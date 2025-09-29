00eba6ef332745b34719edb0e1a452751e802aa4
sathi <sathi@sathish>
2025-09-29 14:51:07 +0530

    encapsulation basic program

---
+++ Tnsif_java_thanush/src/oops_encapsulation/OopsConceptDemo.java
@@ -0,0 +1,42 @@
+package oops_encapsulation;
+
+public class OopsConceptDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		private int serialNum; // int, string, boolean, float = Primitive Data types
+		private String name; // private , public, protected and default = Access modifiers / specifiers
+		private int age;
+
+		// Getters and Setters method
+		public int getSerialNum() { // is used to get the var name & its return value
+			return serialNum;
+		}
+
+		public void setSerialNum(int serialNum) { // is used to set the var value
+			this.serialNum = serialNum;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+
+		// Object class method - to returns string representation of the object
+		@Override
+		public String toString() {
+			return "OopsConceptDemo [Serial Number=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+
+}
